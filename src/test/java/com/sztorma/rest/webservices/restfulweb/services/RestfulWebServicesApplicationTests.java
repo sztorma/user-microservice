bd28f55e38ca4e1b40c76673279185851af59ed4
Szabolcs Torma <szabolcstorma@Szabolcs-MacBook-Pro.local>
2022-06-20 13:51:36 +0200

    init project

---
+++ src/test/java/com/sztorma/rest/webservices/restfulweb/services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sztorma.rest.webservices.restfulweb.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
