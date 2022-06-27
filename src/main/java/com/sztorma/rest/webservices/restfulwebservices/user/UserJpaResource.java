7e254c650faf1af0755d8f67ef5373af7a84a90e
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-06-27 09:26:15 +0200

    user jpa resource

---
+++ src/main/java/com/sztorma/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -0,0 +1,61 @@
+package com.sztorma.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/jpa")
+public class UserJpaResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+        WebMvcLinkBuilder linkToUsers = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        EntityModel<User> userModel = EntityModel.of(user.get());
+        userModel.add(linkToUsers.withRel("all-users"));
+        return userModel;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
+        }
+    }
+
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedUser.getId())
+            .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ src/test/java/com/sztorma/rest/webservices/restfulwebservices/user/UserJpaResourceIT.java
@@ -0,0 +1,104 @@
+package com.sztorma.rest.webservices.restfulwebservices.user;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserJpaResourceIT {
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    private static String stringDate;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    public static void setup() {
+        stringDate = DATE_FORMAT.format(new Date());
+    }
+
+    @Test
+    @DisplayName("Retrieve all jpa users")
+    public void testRetrievAllUsers() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/jpa/users"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value("1"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Jack"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].birthDate").value("1992-04-16"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value("2"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Jill"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[1].birthDate").value("1992-03-15"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value("3"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value("Jam"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[2].birthDate").value("1999-08-16"));
+    }
+
+    @Test
+    @DisplayName("Retrieve one jpa user")
+    public void testRetrieveOneUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/jpa/users/2"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("2"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Jill"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.birthDate").value("1992-03-15"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$._links").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$._links").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("User not found")
+    public void testRetrieveNoUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/200"))
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Delete user")
+    public void testDeleteUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/users/1"))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @DisplayName("Delete non existing user")
+    public void testDeleteNonExistingUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/users/10000"))
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Save new user")
+    public void testSaveNewUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"Csabi\", \"birthDate\": \"2001-11-14\"}"))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andExpect(header().string("Location", "http://localhost/users/4"));
+    }
+
+    @Test
+    @DisplayName("Attempt save invalid user")
+    public void testAttemptSaveInvalidUser() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\": \"s\", \"birthDate\": \"3001-11-14\"}"))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.invalidParams.name").value("name is too short at least 2 characters required"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.invalidParams.birthDate").value("Can not be future"));
+    }
+
+}
